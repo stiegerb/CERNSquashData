46679eb6acad9b3233b20e7f54964a3c55323fab
Benjamin Stieger <stiegerb@cern.ch>
2016-06-14 15:21:57 +0200

    Get size of division by rows, not by columns (more robust)

--- squash_crawl.py
+++ squash_crawl.py
@@ -52,9 +52,19 @@ def get_name(tag):
 def get_result(tag):
 	res = re.match(r'(\d)-(\d)', tag.get_text().strip()) # '3-1', '2-3', etc.
 	if res:
-		return tag.get_text().strip()
+		return str(tag.get_text().strip())
 	else:
 		return EMPTYRESULT
+
+def invert_result(result):
+	if result == EMPTYRESULT: return result
+	if result in ['1', '2', '3']: return '0'
+	score = re.match(r'(\d)-(\d)', result)
+	try:
+		return '%d-%d' % (int(score.group(2)), int(score.group(1)))
+	except ValueError, e:
+		print "Wrong result format? %s" % result
+		raise e
 
 def get_division(tag):
 	# Extract the division rank
@@ -66,9 +76,7 @@ def get_division(tag):
 	rank = int(rank_match.group(1))
 	return rank
 
-
-
-def process_page(url, printout=False):
+def process_page(url, printout=False, verbose=False):
 	## Load page
 	print '... processing %s' % url
 	page_request = urllib2.urlopen(url)
@@ -76,89 +84,82 @@ def process_page(url, printout=False):
 	## Parse html
 	soup = BeautifulSoup(page_request, 'html.parser')
 
-	players   = {} # div rank -> list of player names
+	divisions   = {} # div rank -> list of player names
 	results   = {} # player name -> list of results
 	matches   = {} # (name1, name2) -> result
-	division_size = -1
 
-	## First pass: collect all divisions and players in each division
+	## Some seasons use a sub file to store the results
 	if not len(soup.find_all('table')):
-		## Some seasons use a sub file to store the results
 		frame = soup.find('link', href=re.compile(r'sheet[\d]+?\.htm'))
 		suburl = '%s/%s' % (url.rsplit('/',1)[0], frame['href'])
 		return process_page(suburl)
 
+	## First pass: collect all divisions and players in each division
+	rank = -1
+	player_rows = {} # name -> row
 	for table in soup.find_all('table'):
 		rows_iter = iter(table.find_all('tr'))
 		for row in rows_iter:
 			# Check if this starts a new division or not
 			div = row.find('td', string=re.compile('Division'))
-			if not div: continue
-			rank = get_division(div)
+			if div:
+				rank = get_division(div)
 
-			# Extract the division size (once)
-			if division_size < 0:
-				res_cols = row.find_all('td', string=re.compile(r'\b[A-G]{1}\b'))
-				division_size = len(res_cols)
+			# Player rows always start with a field containing A,B,C,...
+			elif row.find('td', string=re.compile(r'\b[A-G]{1}\b')):
+				# Player entry is then the second one in the row
+				player_name = get_name(row.find_all('td')[1])
+				divisions.setdefault(rank,[]).append(player_name)
+				player_rows[player_name] = row
 
-			# print 'Division %d (%d players)' % (rank, division_size)
+	## Second pass: now that we know the number of players in each division,
+	##              extract the results from the rows.
+	for divrank, player_names in divisions.iteritems():
+		division_size = len(player_names)
+		if verbose:
+			print 'Division %d with %d players:' % (divrank, len(player_names)),
+			print ' %s' % ', '.join(player_names)
 
-			# Store the next N rows as player rows
-			prows = [next(rows_iter) for _ in range(division_size)]
+		# Go through the rows and extract the match results
+		for name1 in player_names:
+			# Results start in 3rd position and we know how many to expect
+			# However we can start at the position of the player+1 to avoid
+			# double counting the matches.
+			pos = player_names.index(name1)
+			match_results = player_rows[name1].find_all('td')[2:division_size+2]
+			for pos2,entry in zip(range(pos+1, division_size), match_results[pos+1:]):
+				name2 = player_names[pos2]
+				result = get_result(entry)
+				matches[(name1, name2)] = result
+				results.setdefault(name1, []).append(result)
+				results.setdefault(name2, []).append(invert_result(result))
+				if verbose:
+					print '  %-30s vs. %-30s:' % (name1,name2),
+					print result
 
-			# Player entry is always the second one in the row
-			players[rank] = [get_name(r.find_all('td')[1]) for r in prows]
-
-			# print '     players: %s' % ', '.join(players[rank])
-
-			# Go through the rows and extract the match results
-			for name,prow in zip(players[rank], prows):
-				# Results start in 3rd position and we know how many to expect
-				# However we can start at the position of the player+1 to avoid
-				# double counting the matches.
-				pos = players[rank].index(name)
-				match_results = prow.find_all('td')[2:division_size+2]
-
-				for pos2,entry in zip(range(pos+1, division_size), match_results[pos+1:]):
-					player2_name = players[rank][pos2]
-					# print '  %-30s vs. %-30s:' % (name,player2_name),
-					result = get_result(entry)
-					# print result
-					matches[(name, player2_name)] = result
-
-				for entry in match_results:
-					result = get_result(entry)
-					results.setdefault(name, []).append(result)
 
 	# Remove empty player names
-	for divrank,names in players.iteritems():
-		players[divrank] = [n for n in names if len(n)]
+	for divrank,names in divisions.iteritems():
+		divisions[divrank] = [n for n in names if len(n)]
 	# Remove divisions with no players
-	players = {r:ps for r,ps in players.iteritems() if len(ps)}
+	divisions = {r:ps for r,ps in divisions.iteritems() if len(ps)}
 	results.pop('', None)
 	matches.pop(('',''), None)
 	# Remove empty matches
 	matches = {k:m for k,m in matches.iteritems() if m != EMPTYRESULT}
 
-	print ('  %d divisions of size %d, (%d played matches)' %
-		       (len(players), division_size, len(matches)))
+	print ('  %2d divisions, %3d players, %3d played matches' %
+		       (len(divisions), len(results), len(matches)))
 
-	try:
-		# Make sure nothing was filled in the -1 rank
-		assert players.get(-1, None) is None
-		# Make sure we found a division size
-		assert division_size > 0
-		# Make sure all divisions are the same size
-		# assert all([len(p) == division_size for p in players.values()])
-	except AssertionError:
-		if division_size > 0:
-			pprint(players)
-		else:
-			print "No valid divisions found"
-			raise RuntimeError('Invalid format')
+	if verbose:
+		for divrank,names in sorted(divisions.iteritems()):
+			print '----- Division %d' % divrank
+			for name in names:
+				print '%-30s: ' % name, results[name]
+
 
 	if printout:
-		for divrank, names in players.iteritems():
+		for divrank, names in divisions.iteritems():
 			print 50*'-'
 			print 'Division %d' % divrank
 			for name in names:
@@ -166,7 +167,7 @@ def process_page(url, printout=False):
 		print 50*'-'
 
 
-	return players, matches
+	return divisions, matches
 
 def process_archives(url):
 	## Load page
@@ -216,16 +217,9 @@ def process_archives(url):
 	print 'Found %d individual players' % len(all_players)
 	print 'Failed for %d sites:' % len(failed_sites), failed_sites
 	print 40*'='
-	# for name in sorted(all_players): print repr(name)
-	# with open('names.txt', 'w') as ofile:
-	# 	for name in all_players:
-	# 		ofile.write('%s\n'%name)
-	# 	ofile.write('\n')
-	# print 40*'='
 
 def main():
 	get_name_dictionary('player_names.txt')
-	# pprint(PNAMES)
 	process_archives('http://club-squash.web.cern.ch/club-squash/resultats.html')
 
 
