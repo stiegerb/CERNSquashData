ed2b89e26058d81b019d916ea6de2d4e14ca4658
Benjamin Stieger <stiegerb@cern.ch>
2016-06-16 01:31:27 +0200

    Added printing of elo rating

--- print_data.py
+++ print_data.py
@@ -3,14 +3,17 @@ import sys
 import json
 
 def print_player(data, mute=False):
-	message = '{name:<30} -- Total games: {tot:3} ({wins:3} wins, {rate:.2%} percentage)'
+	message = '{name:<30} | {elo:6.1f} | {tot:4} | {wins:4} | {rate:.2%} | {seasons:4} |'
 	if not mute:
 		print message.format(
 					 name=name,
 		             tot=data['n_total_matches'],
 		             wins=data['n_total_wins'],
-		             rate=float(data['n_total_wins'])/data['n_total_matches'])
-	return float(data['n_total_wins'])/data['n_total_matches']
+		             rate=float(data['n_total_wins'])/data['n_total_matches'],
+		             elo=data.get('last_elo', 1000),
+		             seasons=data['n_seasons_played'])
+
+	return float(data['n_total_wins'])/data['n_total_matches'], data.get('last_elo', 1000)
 
 squash_data = None
 with open('squash_data.json', 'r') as ifile:
@@ -28,5 +31,9 @@ for name, data in squash_data['players'].iteritems():
 	if data['n_total_matches'] < CUTOFF: continue
 	winrates.append((name,print_player(data, mute=True)))
 
-for name,winrate in sorted(winrates, key=lambda x: x[1]):
+header = "Player name                    | Elo    | Tot  | Win  | WRate  | Seas |"
+print header
+print len(header)*"-"
+for name,(winrate, elo) in sorted(winrates, key=lambda x: x[1][1], reverse=True):
 	print_player(squash_data['players'][name], mute=False)
+print len(header)*"-"
