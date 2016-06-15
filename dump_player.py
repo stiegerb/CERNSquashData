0e2f092597f025e0f50a90a387f49862c95bf2d5
Benjamin Stieger <stiegerb@cern.ch>
2016-06-16 00:03:24 +0200

    Print all the matches in dump_player.py

--- dump_player.py
+++ dump_player.py
@@ -19,8 +19,18 @@ print '{name:<30} -- Total games: {tot} ({wins} wins, {rate:.2%} percentage)'.fo
 	             wins=player_data['n_total_wins'],
 	             rate=float(player_data['n_total_wins'])/player_data['n_total_matches'])
 
+## Dump to json file
 oname = 'player_data_%s.json' % name.replace(' ', '').lower()
 with open(oname, 'w') as ofile:
 	json.dump(player_data, ofile, indent=2, sort_keys=True)
 
 
+## Print all the matches
+all_matches = []
+for season,seas_data in player_data['seasons'].iteritems():
+	for opp,res in seas_data['matches'].iteritems():
+		all_matches.append((season,opp,res))
+
+print len(all_matches)
+for seas,opp,res in all_matches:
+	print '%-12s: %-30s: %s' % (seas, opp, res)
