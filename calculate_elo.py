1106c9811291aff8069658a4c98842e961c25e08
Benjamin Stieger <stiegerb@cern.ch>
2016-06-16 10:12:50 +0200

    base elo to 1200, calculate elo for each season, make exportable

--- calculate_elo.py
+++ calculate_elo.py
@@ -6,7 +6,7 @@ from operator import itemgetter
 
 from squash_crawl import parse_result
 
-BASEELO = 1000
+BASEELO = 1200
 
 def expected_score(rating_diff):
 	return 1.0 / (1+10**(-1.0*rating_diff/400.))
@@ -32,55 +32,74 @@ def calculate_elo(elo1, elo2, result):
 	elo2_new = elo2 + K * ((1.0-score) - expected_score(elo2-elo1))
 	return elo1_new, elo2_new
 
-## Read the data
-squash_data = None
-with open('squash_data.json', 'r') as ifile:
-	squash_data = json.load(ifile)
+def sort_seasons(sdata):
+	## Sort seasons by date
+	seasons = [(k, v['year'], v['month']) for k,v in sdata.iteritems()]
+	seasons.sort(key=itemgetter(1,2))
+	return seasons
 
-## Set everybody's default elo to the base
-reset = True
-for name in squash_data['players'].keys():
-	if reset:
-		squash_data['players'][name]['last_elo'] = BASEELO
-	else:
-		squash_data['players'][name].setdefault('last_elo', BASEELO)
 
-## Sort seasons by date
-seasons = [(k, v['year'], v['month']) for k,v in squash_data['seasons'].iteritems()]
-seasons.sort(key=itemgetter(1,2))
-max_elo = 1000
-max_name = None
-max_season = None
-for season,_,_ in seasons:
-	sdata = squash_data['seasons'][season]
-	# Keep track of all played matches this season
-	counted_matches = {}
-	for pname in sdata['players']:
-		pdata = squash_data['players'][pname]
-		matches = pdata['seasons'][season]['matches']
-		for pname2, result in matches.iteritems():
-			# Avoid double counting
-			if ((pname, pname2) in counted_matches or
-				(pname2, pname) in counted_matches): continue
-			pdata2 = squash_data['players'][pname2]
+def main():
+	## Read the data
+	squash_data = None
+	with open('squash_data.json', 'r') as ifile:
+		squash_data = json.load(ifile)
 
-			elo1, elo2 = calculate_elo(pdata['last_elo'], pdata2['last_elo'], result)
-			pdata['last_elo']  = elo1
-			pdata2['last_elo'] = elo2
+	## Set everybody's default elo to the base
+	reset = True
+	for name in squash_data['players'].keys():
+		if reset:
+			squash_data['players'][name]['last_elo'] = BASEELO
+		else:
+			squash_data['players'][name].setdefault('last_elo', BASEELO)
 
-			if elo1 > max_elo:
-				max_name = pname
-				max_season = season
-				max_elo = elo1
-			if elo2 > max_elo:
-				max_name = pname2
-				max_season = season
-				max_elo = elo2
+	seasons = sort_seasons(squash_data['seasons'])
+	max_elo, max_name, max_season = BASEELO, None, None
 
-			counted_matches[(pname, pname2)] = None
+	for season,_,_ in seasons:
+		sdata = squash_data['seasons'][season]
+		# Keep track of all played matches this season
+		counted_matches = {}
+		for pname in sdata['players']:
+			pdata = squash_data['players'][pname]
+			matches = pdata['seasons'][season]['matches']
+			elo1 = pdata['seasons'][season].setdefault('elo', pdata['last_elo'])
+			for pname2, result in matches.iteritems():
+				# Avoid double counting
+				if ((pname, pname2) in counted_matches or
+					(pname2, pname) in counted_matches): continue
+				pdata2 = squash_data['players'][pname2]
 
-print max_elo, max_name, max_season
+				elo2 = pdata2['seasons'][season].setdefault('elo', pdata2['last_elo'])
+				elo1, elo2 = calculate_elo(pdata['last_elo'], pdata2['last_elo'], result)
 
-## Dump to json file
-with open('squash_data.json', 'w') as ofile:
-	json.dump(squash_data, ofile, indent=2, sort_keys=True)
+				# Store the latest elo rating
+				pdata['last_elo']  = elo1
+				pdata2['last_elo'] = elo2
+				# Store the elo for this season
+				pdata['seasons'][season]['elo']  = elo1
+				pdata2['seasons'][season]['elo'] = elo2
+
+				if elo1 > max_elo:
+					max_name = pname
+					max_season = season
+					max_elo = elo1
+				if elo2 > max_elo:
+					max_name = pname2
+					max_season = season
+					max_elo = elo2
+
+				counted_matches[(pname, pname2)] = None
+
+
+	print max_elo, max_name, max_season
+
+	## Dump to json file
+	with open('squash_data.json', 'w') as ofile:
+		json.dump(squash_data, ofile, indent=2, sort_keys=True)
+
+
+
+
+if __name__ == '__main__':
+	main()
