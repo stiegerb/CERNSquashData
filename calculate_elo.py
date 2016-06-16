6bd88560b7a628db5f26f7da1146d83ece311c66
Benjamin Stieger <stiegerb@cern.ch>
2016-06-16 02:05:44 +0200

    Reset elo ratings before calculating; added calculation of max elo

--- calculate_elo.py
+++ calculate_elo.py
@@ -38,24 +38,48 @@ with open('squash_data.json', 'r') as ifile:
 	squash_data = json.load(ifile)
 
 ## Set everybody's default elo to the base
+reset = True
 for name in squash_data['players'].keys():
-	squash_data['players'][name].setdefault('last_elo', BASEELO)
+	if reset:
+		squash_data['players'][name]['last_elo'] = BASEELO
+	else:
+		squash_data['players'][name].setdefault('last_elo', BASEELO)
 
 ## Sort seasons by date
 seasons = [(k, v['year'], v['month']) for k,v in squash_data['seasons'].iteritems()]
 seasons.sort(key=itemgetter(1,2))
-
+max_elo = 1000
+max_name = None
+max_season = None
 for season,_,_ in seasons:
 	sdata = squash_data['seasons'][season]
+	# Keep track of all played matches this season
+	counted_matches = {}
 	for pname in sdata['players']:
 		pdata = squash_data['players'][pname]
 		matches = pdata['seasons'][season]['matches']
 		for pname2, result in matches.iteritems():
+			# Avoid double counting
+			if ((pname, pname2) in counted_matches or
+				(pname2, pname) in counted_matches): continue
 			pdata2 = squash_data['players'][pname2]
+
 			elo1, elo2 = calculate_elo(pdata['last_elo'], pdata2['last_elo'], result)
 			pdata['last_elo']  = elo1
 			pdata2['last_elo'] = elo2
 
+			if elo1 > max_elo:
+				max_name = pname
+				max_season = season
+				max_elo = elo1
+			if elo2 > max_elo:
+				max_name = pname2
+				max_season = season
+				max_elo = elo2
+
+			counted_matches[(pname, pname2)] = None
+
+print max_elo, max_name, max_season
 
 ## Dump to json file
 with open('squash_data.json', 'w') as ofile:
